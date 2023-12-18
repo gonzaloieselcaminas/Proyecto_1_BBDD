8c93a3cdb28d346cf9e2a4bf3a92e055f63c25b1
alu10639143 <alusan9143@ieselcaminas.org>
2023-12-18 15:18:09 +0100

    Primer Commit

---
+++ src/main/java/com/example/proyecto_1_bbdd/Proyecto1BbddApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_1_bbdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto1BbddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Proyecto1BbddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/proyecto_1_bbdd/Proyecto1BbddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_1_bbdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto1BbddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
