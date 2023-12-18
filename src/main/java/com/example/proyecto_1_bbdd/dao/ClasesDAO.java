5425ecc9fa0516dbbb31a9caf1d32b949a4d3e89
alu10639143 <alusan9143@ieselcaminas.org>
2023-12-18 16:24:49 +0100

    DAOs hechos

---
+++ src/main/java/com/example/proyecto_1_bbdd/dao/ClasesDAO.java
@@ -0,0 +1,7 @@
+package com.example.proyecto_1_bbdd.dao;
+
+import com.example.proyecto_1_bbdd.entity.Clases;
+
+public interface ClasesDAO {
+    void save(Clases clases);
+}
---
+++ src/main/java/com/example/proyecto_1_bbdd/dao/UsersAdminDAO.java
@@ -0,0 +1,7 @@
+package com.example.proyecto_1_bbdd.dao;
+
+import com.example.proyecto_1_bbdd.entity.UsersAdmin;
+
+public interface UsersAdminDAO {
+    void save(UsersAdmin usersAdmin);
+}
