98e0cdabc778715d8c21f4195c5034907613637e
alu10513394 <alulop3394@ieselcaminas.org>
2023-12-20 20:43:23 +0100

    estamos haciendo las relaciones..|.

--- src/main/java/com/example/proyecto_1_bbdd/models/entity/Horario.java
+++ src/main/java/com/example/proyecto_1_bbdd/models/entity/Horario.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 import java.sql.Date;
 import java.sql.Time;
+import java.util.List;
 
 
 @Entity
@@ -17,15 +18,16 @@ public class Horario {
     public Horario() {
     }
 
-    public Horario(int id_clase, Time horaInicio, Time horaFinal, Date fecha) {
-        this.id_clase = id_clase;
+    public Horario(List<Clases> clases, Time horaInicio, Time horaFinal, Date fecha) {
+        this.clases = clases;
         this.horaInicio = horaInicio;
         this.horaFinal = horaFinal;
         this.fecha = fecha;
     }
 
-    @Column(name = "id_clase", insertable=false, updatable=false)
-    private int id_clase;
+    @OneToMany
+    @JoinColumn(name = "id_clase")
+    private List<Clases> clases;
 
     @Column(name = "horaInicio")
     private Time horaInicio;
@@ -34,16 +36,12 @@ public class Horario {
     @Column(name = "fecha")
     private Date fecha;
 
-    @ManyToOne
-    @JoinColumn(name = "id_clase", referencedColumnName = "id")
-    private Clases clase;
-
     public int getId_horario() {
         return id_horario;
     }
 
-    public int getId_clase() {
-        return id_clase;
+    public List<Clases> getClases() {
+        return clases;
     }
 
     public Time getHoraInicio() {
@@ -62,8 +60,8 @@ public class Horario {
         this.id_horario = id_horario;
     }
 
-    public void setId_clase(int id_clase) {
-        this.id_clase = id_clase;
+    public void setClases(List<Clases> clase) {
+        this.clases = clases;
     }
 
     public void setHoraInicio(Time horaInicio) {
@@ -77,13 +75,4 @@ public class Horario {
     public void setFecha(Date fecha) {
         this.fecha = fecha;
     }
-
-    public Clases getClase() {
-        return clase;
-    }
-
-    public void setClase(Clases clase) {
-        this.clase = clase;
-    }
-
 }
