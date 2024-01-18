abb7a96bd9e1421ffd29ab9084bf571bd62e9aac
alu10513394 <alulop3394@ieselcaminas.org>
2024-01-18 17:00:51 +0100

    con roles

--- src/main/java/com/example/proyecto_1_bbdd/models/entity/Cursos.java
+++ src/main/java/com/example/proyecto_1_bbdd/models/entity/Cursos.java
@@ -6,7 +6,7 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
-@Table(name = "Cursos")
+@Table(name ="Cursos")
 public class Cursos {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
