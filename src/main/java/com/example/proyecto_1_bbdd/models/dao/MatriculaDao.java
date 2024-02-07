d0050e673f0501f38143ac161d38439c3917047e
alu10513394 <alulop3394@ieselcaminas.org>
2024-02-07 20:48:51 +0100

    acabado las seeds

--- src/main/java/com/example/proyecto_1_bbdd/models/dao/MatriculaDao.java
+++ src/main/java/com/example/proyecto_1_bbdd/models/dao/MatriculaDao.java
@@ -5,6 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface MatriculaDao extends JpaRepository<Matricula, Long> {
-
-}
+public interface MatriculaDao extends JpaRepository<Matricula, Long> { }
